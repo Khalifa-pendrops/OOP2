b36a3840c7092fb676db523f2d1552b879c6e379
khalifa-pendrops <dikee5200@gmail.com>
2025-01-23 14:29:58 +0100

    OOP2.js file added and program instantiated

---
+++ oop2.js
@@ -0,0 +1,70 @@
+class System {
+  #Id;
+  constructor(name, Id, grade) {
+    this.name = name;
+    this.grade = grade;
+    this.#Id = Id; //this is private, explaining encapsulation
+  }
+
+  addStudent(student) {
+    console.log(`${student} has been added to the system.`);
+  }
+
+  studentDetails() {
+    console.table(
+      `Student Details: Name: ${this.name}, ID: ${this.#Id}, Grade: ${
+        this.grade
+      }`
+    );
+  }
+
+  // abstraction here!
+  averageGrade(totalScore, numberOfSubjects) {
+    throw new Error("Forget it, not for this System class");
+  }
+}
+
+class ReturningStudent extends System {
+  constructor(name, Id, grade, bestSubject) {
+    super(name, Id, grade);
+  }
+
+  specialty() {
+    console.log(`${this.name} is a science student.`);
+  }
+
+  averageGrade(totalScore, numberOfSubjects) {
+    const result = totalScore / numberOfSubjects;
+    console.log(`Average grade: ${result}`);
+  }
+}
+
+class NewStudent extends System {
+  constructor(name, Id, grade) {
+    super(name, Id, grade);
+  }
+
+  averageGrade(totalScore, numberOfSubjects) {
+    console.log(`${this.name} has no average grade, he/she is a new Student.`); //POLYMORPHISM
+  }
+}
+
+// INSTANCE OF RETURNING STUDENT
+const returningStudent = new ReturningStudent("Khalifa", 101, 100, "Chemistry");
+
+returningStudent.addStudent("Khalifa");
+
+returningStudent.studentDetails();
+
+returningStudent.averageGrade(88, 9);
+
+returningStudent.specialty();
+
+// INSTANCE OF NEW STUDENT
+const newStudent = new NewStudent("Ogechi", 111, undefined);
+
+newStudent.addStudent("Ogechi");
+
+newStudent.studentDetails();
+
+newStudent.averageGrade();
